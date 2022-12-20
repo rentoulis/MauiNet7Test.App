4168016b3d45c7ccc4f01f2c37baed688f2c3ea7
rentoulis <96864128+rentoulis@users.noreply.github.com>
2022-12-20 11:07:32 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace MauiNet7Test;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("MyCustomization", (handler, view) =>
+        {
+#if ANDROID
+            handler.PlatformView.SetPadding(0, 0, 0, 0);
+#endif
+        });
+
+        MainPage = new AppShell();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiNet7Test;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
